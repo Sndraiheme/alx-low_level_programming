67f6644a329f129e745f6049118c217afae637b5
Sndraiheme <sandraiheme93@gmail.com>
2023-02-16 18:50:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h
+#include <stdio.h>
 /**
  * main- Serve as the entry point for the program
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
